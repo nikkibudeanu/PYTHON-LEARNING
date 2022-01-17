1925cc836f2ce97ad87230af70fff00d6a5b69ae
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-17 13:16:48 +0000

    learn if else

--- main.py
+++ main.py
@@ -165,6 +165,12 @@ print ( "ink" not in "sink")
 print("robbie" in  ["gary", "howard", "mark", "jason"])
 
 
+# if/else statement
+number = int(input("Please enter a number:"))
 
+if number == 5:
+    print(f"{number} is equal to 5")
+else:
+    print(f"{number} is not equal to 5")
 
 
