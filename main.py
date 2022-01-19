83e3d4cec9f1a2726ef4b7309f9aeb2118cd3e5f
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-19 17:22:47 +0000

    learn nested iteration

--- main.py
+++ main.py
@@ -291,3 +291,15 @@ for number in range(10):
     print(f'Number is  {number}')
 
 print('Left the loop') 
+
+# nested iteration
+i = 2
+while i < 10:
+    j = 2
+    while j <= i/j:
+        if not i % j:
+            break
+        j += 1
+    if j > i/j:
+        print(f'{i} is a prime number')
+    i += 1
