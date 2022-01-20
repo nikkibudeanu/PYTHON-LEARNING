c1f8cc96c12409f8e51c4fe3135fbf9b038d88d9
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-20 17:12:18 +0000

    learn dictionary comprehension

--- main.py
+++ main.py
@@ -847,6 +847,24 @@ print(evens)
 evens = [num for num in numbers if num % 2 == 0]
 
 
+#Dictionary Comprehensions
+
+#Dictionary comprehensions are just like list comprehensions. The difference in the syntax is that curly rather than square brackets are used. Also, before the for keyword you need to include the key and value separated with a colon.
+
+# 1. {'cherry': 6, 'mango': 5, 'apple': 5, 'banana': 6}
+fruits = ['apple', 'mango', 'banana','cherry']
+print({f:len(f) for f in fruits})
+
+# 2. {0: '', 1: '*', 2: '**', 3: '***', 4: '****'}
+print({i:(i*'*') for i in range(0,5)})
+
+# 3. {0: True, 1: False, 2: True, 3: False, 4: True, 5: False, 6: True, 7: False, 8: True, 9: False}
+print({i:(True if i%2==0 else False) for i in range(10)})
+
+# 4. {(0, 1): False, (1, 2): False, (3, 2): False, (0, 0): True, (3, 3): True, (3, 0): False, (3, 1): False,
+# (2, 1): False, (0, 2): False, (2, 0): False, (1, 3): False, (2, 3): False, (2, 2): True, (1, 0): False,
+# (0, 3): False, (1, 1): True}
+print({(i,j): (True if i==j else False) for i in range(4) for j in range(4)})
 
 
 
