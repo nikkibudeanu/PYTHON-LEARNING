a9709937acff6eb5ad9af8e07057f1aa898fced3
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-21 09:37:57 +0000

    learn call a function

--- main.py
+++ main.py
@@ -1032,3 +1032,31 @@ print(add(5, 12))
 
 
 
+#call a function
+
+# Calling a function is executing the code. 
+# When you defined a function, you used the def keyword but to call 
+# it you simply use the function name followed by parentheses. 
+# You can pass information into the function as arguments inside the 
+# parentheses separated by commas. 
+# A parameter is the variable listed in the parentheses when the function is 
+# defined, and the argument is the value you pass into the function 
+# parentheses when it is called. You have to supply the same number of arguments 
+# as there are parameters. A function can be called by another function or even by itself.
+
+
+# 2. This function runs for the name and age function calls
+def get_user_input(prompt):
+    return input(prompt)
+
+# 4. This function runs twice
+def print_out_to_console(value_to_be_printed):
+    print(value_to_be_printed)
+
+# 1. name and age are the first two function calls to run sequentially
+name = get_user_input("Input your name:")
+age = get_user_input("Input your age:")
+
+# 3. Then function calls run sequentially
+print_out_to_console(f"Your name is {name}")
+print_out_to_console(f"You are {age} years old")
