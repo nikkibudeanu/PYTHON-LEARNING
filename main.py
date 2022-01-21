caa3c1bdaa7e426a2a016384f9e8599640117ad1
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-21 09:07:20 +0000

    learn identation in Python

--- main.py
+++ main.py
@@ -989,6 +989,24 @@ def __init__(self, realpart, imagpart):
     self.i = imagpart
 
 
+#Indentation in Python
+# Correct:
+
+# It is aligned with the opening delimiter, e.g. the opening parentheses.
+foo = long_function_name(var_one, var_two,
+                         var_three, var_four)
+
+# Add four spaces (an extra level of indentation) to distinguish arguments from the rest.
+def long_function_name(
+        var_one, var_two, var_three,
+        var_four):
+    print(var_one)
+
+# Hanging indents should add a level.
+foo = long_function_name(
+    var_one, var_two,
+    var_three, var_four)
+
 
 
 
