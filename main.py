c62e543e5638c028f5bd7df63c274b0dceb19373
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-22 14:19:50 +0000

    learn classes

--- main.py
+++ main.py
@@ -1425,4 +1425,23 @@ def get_odd_numbers(numbers):
     
 print(get_odd_numbers(numbers))
 
+# classes
+
+#Classes are a good way of combining data and methods and are useful when dealing with hierarchies. 
+# They are generally used to model more complex data types which can’t be 
+# modelled using Python’s built-in data structures such as lists and dicts.
+
+#We define a class by using the class keyword followed by the name. 
+# Like functions, a class does not do anything until it is executed. 
+# Scope also applies to classes so global and nonlocal scope have to be taken into consideration. 
+# A class name should start with a capital letter and should have a docstring. 
+# Classes can contain functions and other statements.
+
+class HelloWorld:
+    """A simple example class"""
+    i = 12345
+
+    def f(self):
+        return 'Hello, world!'
+
 
