2a4561ad071bbf37159ed99ea2437a0794792a29
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-22 17:52:46 +0000

    challenge - class properties and attributes

--- main.py
+++ main.py
@@ -1656,6 +1656,24 @@ print(Bird.definition)
 print(Bird.call)
 
 
+# challenge class properties and attributes
+class ContactInfo():
+    """
+    Creates an instance of ContactInfo
+    """
+    about =  "Contact information for customer"
+    
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+    
+    def description(self):
+        return f"{self.name} : {self.email}" 
+
+print(ContactInfo.about)  
+
+contact = ContactInfo('dave', 'lister@email.com')
+print(contact.description())
 
 
 
