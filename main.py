b255df0c43fa280d38db2d21ed97f3183a481fe9
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-22 16:51:15 +0000

    challenge class instances

--- main.py
+++ main.py
@@ -1593,6 +1593,28 @@ print(owl.description())
 del owl.call
 print(owl.description())
 
+# challenge instances
+class User():
+    """
+    Creates an instance of User
+    """
+    def __init__(self, username, email, subscribed):
+        self.username = username
+        self.email = email
+        self.subscribed = subscribed
+    
+    def description(self):
+        """
+        Describes the instance of User
+        """
+        return f"user: {self.username}, email: {self.email}, subscribed: {self.subscribed}"
+
+
+# write your code here
+user_one = User("arnold", "arnold@email.com", True)
+print(user_one.email)
+user_one.email = "murphy@email.com"
+print(user_one.description())
 
 
 
