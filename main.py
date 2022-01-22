70aac0c42e18f4341f5c3671a282f0d317b10685
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-22 18:05:51 +0000

    challenge - methods challenge

--- main.py
+++ main.py
@@ -1717,6 +1717,27 @@ class Bird:
 owl = Bird('owl', 'Twit Twoo!')
 print(owl.description())
 
+# methods challenge 
+class BlogPost():
+    """
+    Creates an instance of BlogPost
+    """
+    def __init__(self, title, content, author):
+        self.title = title
+        self.content = content
+        self.author = author
+    
+    def get_overview(self):
+        return f"{self.title} by {self.author}"
+    
+    def full_info(self):
+        return f"Blog post: {self.title}. Content: {self.content}. Author: {self.author}"
+        
+post = BlogPost("Python Classes", "Python is known as an object-oriented language...", "Code Institute")
+print(post.get_overview())
+print(post.full_info())
+
+
 
 
 
