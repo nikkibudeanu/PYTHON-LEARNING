69a7546b4e06fdef9758c8b047e8238543b056e9
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-22 10:19:46 +0000

    learn taking paramenters

--- main.py
+++ main.py
@@ -1070,3 +1070,35 @@ def print_hello_world():
 print_hello_world()
 
 
+#Taking Parameters & Returning Results
+
+#A function's parameters are the special variables used by a function to handle this input, 
+# whereas the arguments are the values provided for the parameters when we run the function. 
+# For example, on line 1, we create a function that has a parameter named name, and then on line 5 
+# we run that function and pass in the value of the variable username as the argument for that parameter.
+
+def print_message(name):
+    print(f"Hello {name}")
+
+username = input("What's your name? ")
+print_message(username)
+
+#We don’t provide an argument when we invoke the function as we do on line 5, the function will just print out Hello World. 
+# Notice that we initially stored the value in a variable called username, but inside the function, we’ve called in a name. 
+# This is because we’re passing a new variable to the function called name, with the value of username. 
+# The name variable cannot be accessed outside of the function. 
+# Therefore if we were to try to print name outside of the function, it wouldn’t work. 
+# This is known as variable scope.
+
+#Rather than printing the result in the function, we have returned it. 
+# This is how a computer program expects to see the result of a function. 
+# However, to see the output as a human, we then need to wrap the function 
+# calls in print() , so the result is printed to the terminal.
+
+def print_message(name="World"):
+    return f"Hello {name}"
+
+username = input("What's your name? ")
+print(print_message())
+print(print_message(username))
+
