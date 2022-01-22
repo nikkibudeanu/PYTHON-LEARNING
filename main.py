911c63fb8e75580757581775744a1c6ce35cb4ee
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-22 16:35:08 +0000

    learn class instances

--- main.py
+++ main.py
@@ -1544,11 +1544,54 @@ order_item_two = OrderItem("Fez", 25, 1)
 print(order_item_two.description())
 
 
+#Creating Class Instances
+
+#You can create multiple instances of a class. 
+# An instance is an individual object of the class in memory. 
+# It exists live in RAM until the point it is removed.
+
+#Let's return to the Bird class. 
+# When you create an instance of the class, it is the initializer code that runs. 
+# This is the __init__ method. This method assigns the values to the object properties.
+#  The self keyword is a reference to the current instance of the class and is used to access variables that belong to the class. 
+# To create an instance of the class, you need to provide the same quantity of values as there are properties (in this case two). 
+#  Once you have an instance of that class, you can access the variables and methods using the dot notation.
 
 
 
+owl = Bird('Owl', 'Twit Twoo!')
+owl.call
+owl.description()
 
 
+class Bird:
+   """
+   Bird class
+   """
+   def __init__(self, kind, call):
+      #properties
+       self.kind = kind
+       self.call = call
+
+   #behaviour
+   def description(self):
+       """
+       describe the bird
+       """
+       return f"A {self.kind} goes {self.call}" 
+  
+   def bird_call(self):
+       print(self.call.upper())
+
+owl = Bird('Owl', 'Twit Twoo!')
+print(owl.call)
+print(owl.description())
+crow = Bird('Crow', 'Caaaw!')
+print(crow.description())
+owl.call = 'screech'
+print(owl.description())
+del owl.call
+print(owl.description())
 
 
 
