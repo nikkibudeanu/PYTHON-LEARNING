7ec30b2cdbce91bb0bb383c7a7f30c6146629a70
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-23 08:30:48 +0000

    challenge - subclasses

--- main.py
+++ main.py
@@ -1778,7 +1778,30 @@ parrot = Parrot('blue')
 print(parrot.color)
 print(parrot.description())
 
+# challenge subclasses
+class JobListing():
+    """
+    Creates an instance of JobListing
+    """
+    def __init__(self, job_title, department):
+        self.job_title = job_title
+        self.department = department
+    
+    def description(self):
+        return f"Job opening for {self.job_title} in {self.department} department"
 
+
+# write your code here
+class SalesManager(JobListing):
+    def __init__(self, salary):
+        JobListing.__init__(self, "Sales Manager", "Sales")
+        self.salary = salary
+        
+
+sales_manager = SalesManager(45000)
+print(sales_manager.description())
+print(sales_manager.salary)
+            
 
 
 
