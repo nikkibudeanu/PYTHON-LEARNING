40ae89b15593593ba3d48eb0f9f1bad5a16bb263
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-23 08:05:56 +0000

    learn subclasses

--- main.py
+++ main.py
@@ -1738,6 +1738,45 @@ print(post.get_overview())
 print(post.full_info())
 
 
+#Subclassing & Inheritance
+
+#Subclassing is a useful way of creating a specialised version of a class with its methods but re-using existing methods and 
+# properties of the parent (or base) class. 
+# We could create a Parrot class which subclasses the Bird class created in previous units. 
+# We can use the existing methods on the parent/base class, and we don’t have to supply the bird kind or call because that’s 
+# coded into the Parrot class. 
+# To subclass/inherit from a superclass/parent, add the name of the parent class inside parentheses as part of the subclass name. 
+# To call a method on the parent class, precede the parent method name with the parent class name and a period.
+
+
+class Bird:
+    """
+    Bird class
+    """
+    # class attribute
+    definition = "a warm-blooded egg-laying vertebrate animal distinguished by the possession of feathers, wings, a beak, and typically by being able to fly."
+
+    def __init__(self, kind, call):
+        # instance attribute
+        self.kind = kind
+        self.call = call
+
+    def description(self):
+        """
+        describe the bird
+        """
+        return f"A {self.kind} goes {self.call} and is {self.definition}" 
+
+
+class Parrot(Bird):
+    def __init__(self, color):
+        Bird.__init__(self, 'Parrot', 'Kah!')
+        self.color = color
+
+
+parrot = Parrot('blue')
+print(parrot.color)
+print(parrot.description())
 
 
 
