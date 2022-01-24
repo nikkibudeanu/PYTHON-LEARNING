a73fd923b54bbf38c447b294b753d56a067e6be5
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-24 10:25:11 +0000

    challenge - super class

--- main.py
+++ main.py
@@ -2288,3 +2288,35 @@ print(gannet.get_description())
 
 duck = Fowl('Mallard', 'Quack!', 'Waterfowl')
 print(duck.description())
+
+# super class challenge
+class BaseOrderItem():
+    """
+    Creates instance of BaseOrderItem
+    """
+    def __init__(self, product, price, quantity):
+        self.product = product
+        self.price = price
+        self.quantity = quantity
+    
+    """
+    Returns total based on price * quantity ordered
+    """
+    def total_price(self):
+        return self.price * self.quantity
+
+# write your code here
+class ShoeOrderItem(BaseOrderItem):
+    """
+    Creates instance of ShoeOrderItem
+    """
+    def __init__(self, product, price, quantity, size):
+        super().__init__(product, price, quantity)
+        self.size = size
+        
+    def describe(self):
+        return f"Order: {self.product} in size {self.size}, quantity: {self.quantity}, total price: {self.total_price()}"
+        
+
+shoe_order = ShoeOrderItem("Ladies Nike Trainers", 40, 2, 42)
+print(shoe_order.describe())
