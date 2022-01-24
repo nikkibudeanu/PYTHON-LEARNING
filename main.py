044aacb6c5162ff1ee56cb0265e090fbca648d6a
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-24 10:35:14 +0000

    learn passing methods

--- main.py
+++ main.py
@@ -2320,3 +2320,38 @@ class ShoeOrderItem(BaseOrderItem):
 
 shoe_order = ShoeOrderItem("Ladies Nike Trainers", 40, 2, 42)
 print(shoe_order.describe())
+
+# Passing Methods Around
+
+#One thing you may have noticed in the examples is that methods are passed around from superclass or mixin to subclass. 
+# This is possible in Python as a method is itself an object. 
+# Methods are functions within a class so in the same way functions are passed around so are methods.
+
+class Bird:
+    """
+    Bird class
+    """
+    # class attribute
+    definition = "a warm-blooded egg-laying vertebrate animal distinguished by the possession of feathers, wings, a beak, and typically by being able to fly."
+
+    def __init__(self, kind, call):
+        # instance attribute
+        self.kind = kind
+        self.call = call
+
+    def description(self):
+        """
+        describe the bird
+        """
+        return f"A {self.kind} goes {self.call} and is {self.definition}" 
+
+
+class Parrot(Bird):
+    def __init__(self, color):
+        Bird.__init__(self, 'Parrot', 'Kah!')
+        self.color = color
+
+
+parrot = Parrot('blue')
+print(parrot.color)
+print(parrot.description())
