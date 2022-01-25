784df9c5191ab309546f2902ab8bb374a5d614a0
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-25 19:38:08 +0000

    learn catch specific errors

--- main.py
+++ main.py
@@ -3028,7 +3028,28 @@ try:
 except NameError:
     print("Variable not defined")
 
+#Catching Generic Errors With “as”
 
+
+#The built-in exceptions contain information about the error. 
+# Up till now, we have just displayed the exception in the terminal. 
+# The exception object includes the error message and also additional arguments.
+
+"""
+In the runnable example below there is no file book.txt, so the except block runs. 
+The exception OSError is passed to variable e, so we have access to the arguments. 
+The OSError exception is for operating system errors such as file not found. 
+Then we pass the arguments to a tuple with variable names errno and strerror. 
+These are the numeric error code and corresponding error message respectively. 
+We could also have used the filename argument, for example.
+"""
+
+try:
+    f = open('book.txt')
+    s = f.readline()
+except OSError as e:
+    errno, strerror = e.args
+    print(f"There is an I/O error number, {errno}: {strerror}.")
 
 
 
