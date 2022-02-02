4e941cbfd9b36ca42a8cbde6a8e2489e292b0bc8
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-02-02 20:51:22 +0000

    learn dictionaries

--- youtube.py
+++ youtube.py
@@ -154,3 +154,23 @@ elif op == "*":
 else:
     print("Invalid operator")
 
+
+# dictionaries - store information
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+print(monthConversions["Jan"])
+print(monthConversions.get("Dec"))
+
