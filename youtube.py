104b494f6547d3e4d36e19f6401482fefe868037
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-02-04 11:12:31 +0000

    learn for loops

--- youtube.py
+++ youtube.py
@@ -201,3 +201,19 @@ if out_of_guesses:
 else:
     print("You win!")
 
+# For loops
+friends = ["Jim", "Karen"]
+for friend in friends :
+    print(friend)
+
+for index in range(3, 10) :
+    print(index)
+
+for index in range(len(friends)) :
+    print(index)
+    
+for index in range(5) :
+    if index == 0:
+        print("first iteration")
+    else:
+        print("Not first")
