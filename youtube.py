16f6ce2985da2c010521dbf57d95f4de2669c7b4
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-02-04 10:54:02 +0000

    learn to build a guessing game

--- youtube.py
+++ youtube.py
@@ -182,3 +182,22 @@ while i <= 10:
 
 print("Done with loop")
 
+# Build a basic guessing game
+secret_word = "giraffe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not (out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("Out of Guesses, You Lose")
+else:
+    print("You win!")
+
