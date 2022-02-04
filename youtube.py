6a416af36caa50dafd9434ba074230d855858c4b
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-02-04 11:19:44 +0000

    learn exponent function

--- youtube.py
+++ youtube.py
@@ -211,9 +211,22 @@ for index in range(3, 10) :
 
 for index in range(len(friends)) :
     print(index)
-    
+
 for index in range(5) :
     if index == 0:
         print("first iteration")
     else:
         print("Not first")
+
+# exponent function
+
+# 2 raised to the 3rd power
+print(2**3)
+
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(3, 4))
