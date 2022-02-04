d9b1c9bd971e33d2531d62b67d3796aeb98ccb62
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-02-04 13:29:27 +0000

    learn catching errors with try and except

--- youtube.py
+++ youtube.py
@@ -259,3 +259,16 @@ def translate(phrase):
     return translation
 
 print(translate(input("Enter a phrase:")))
+
+# try except - catching errors
+
+
+try:
+    value = 10 / 0
+    number = int(input("Enter a number:"))
+    print(number)
+except ZeroDivisionError as err:
+    print(err)
+except ValueError:
+    print("Invalid input")
+
