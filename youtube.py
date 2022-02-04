c76e76500bbc350cc3e9d4ce0ed56d965a6ae097
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-02-04 11:46:56 +0000

    learn 2d lists and nested loops

--- youtube.py
+++ youtube.py
@@ -230,3 +230,20 @@ def raise_to_power(base_num, pow_num):
     return result
 
 print(raise_to_power(3, 4))
+
+# 2D lists and nested loops
+
+number_grid = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
+
+# access element inside , first is list and second is column
+print(number_grid[0][0])
+
+for row in number_grid:
+    for col in row:
+        print(col)
+
