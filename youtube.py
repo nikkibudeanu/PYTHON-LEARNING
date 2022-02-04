3a58d17798663a9845a05aeed6a8f64a9dbf9305
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-02-04 11:57:23 +0000

    learn to build a translator

--- youtube.py
+++ youtube.py
@@ -247,3 +247,15 @@ for row in number_grid:
     for col in row:
         print(col)
 
+# build a translator
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower in "aeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter 
